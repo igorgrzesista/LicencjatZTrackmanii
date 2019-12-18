698dd1f0f684b41bef7bf1d70726ce78ad386a8e
igorgrzesista <igorgrzesista@gmail.com>
2019-12-18 20:03:40 +0100

    Added TrackmaNiaUser class

---
+++ LicencjatZTrackmanii/Models/User/TrackmaniaUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LicencjatZTrackmanii.Models.User
+{
+    public class TrackmaniaUser
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Nickname { get; set; }
+        public string Photo { get; set; }
+
+
+    }
+}
